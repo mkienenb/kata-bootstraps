e795876f948c166f6dea62999483b2f2ad09b292
Mike Kienenberger <mkienenb@gmail.com>
2022-02-19 14:27:09 -0500

    feat: new work day simulator should have 3 total gossip with three drivers.

--- java/junit5/src/main/java/org/gamenet/bus/WorkDaySimulation.java
+++ java/junit5/src/main/java/org/gamenet/bus/WorkDaySimulation.java
@@ -8,10 +8,23 @@ import java.util.List;
 import java.util.Set;
 
 public class WorkDaySimulation {
+    private final List<Driver> driverList;
+    private Set<Gossip> totalGossipSet;
+
     public WorkDaySimulation(Driver... drivers) {
+        this.driverList = Arrays.asList(drivers);
+        totalGossipSet = calculateTotalGossip();
+    }
+
+    private Set<Gossip> calculateTotalGossip() {
+        totalGossipSet = new HashSet<>(driverList.size());
+        for (Driver driver : driverList) {
+            totalGossipSet.addAll(driver.getKnownGossipSet());
+        }
+        return totalGossipSet;
     }
 
     Set<Gossip> getTotalGossip() {
-        return Collections.emptySet();
+        return totalGossipSet;
     }
 }
